cd2a5c7da87688edde8866305c701729f4f8fe81
Rocio Garrido <rocio.garrido@wefox.com>
2024-05-23 09:53:12 +0200

    estructura de pantalla

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-properties-page',
   templateUrl: './properties-page.component.html',
   styleUrls: ['./properties-page.component.css']
 })
-export class PropertiesPageComponent implements OnInit {
+export class PropertiesPageComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  onFieldUpdated(field: string, value: string) {
+    console.log({ field, value });
   }
 
 }
